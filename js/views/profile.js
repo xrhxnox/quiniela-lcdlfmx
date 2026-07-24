677bc0d9bb13c2d3e4b7cfb4442573b40a7c8a44
RicardoHdzm <jrhm95@gmail.com>
2026-07-23 20:25:49 -0700

    .

--- js/views/profile.js
+++ js/views/profile.js
@@ -304,7 +304,7 @@ async function renderProfileInternal(container, username) {
 
   // ---------- Favorito / odiado ----------
   const favHatedCard = h("div", { class: "card" }, [
-    h("p", { style: "margin-top:0;text-align:center" }, [h("i", { class: "fa-solid fa-calendar-days" }), " ", h("strong", {}, "Acerca de esta temporada")]),
+    h("p", { style: "margin-top:0;text-align:center" }, [h("i", { class: "fa-solid fa-calendar-days" }), " ", h("strong", {}, "Cuarta Temporada ")]),
     h("div", { class: "grid", style: "grid-template-columns:repeat(auto-fit, minmax(140px, 140px));justify-content:center" }, [
       participantPickCard("Favorito", favorite, "favorite", counts, favorite ? currentNominationMap[favorite.id] : null),
       participantPickCard("Odiado", hated, "hated", counts, hated ? currentNominationMap[hated.id] : null),
